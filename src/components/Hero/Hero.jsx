2bca2bc96fdefd49987694b809d04afb1f1a2963
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-31 14:34:53 +0500

    Add image gsap animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,40 @@
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+
 import './Hero.scss';
-import heroImg from '../../assets/hero.jpg';
+import heroImg from '../../assets/image.png';
 
 const Hero = () => {
+  // refs
+  const imgRef = useRef();
+
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    // animate image
+    tl.fromTo(
+      imgRef.current,
+      {
+        opacity: 0,
+        y: '10%',
+      },
+      {
+        duration: 1,
+        opacity: 1,
+        y: '0%',
+      }
+    );
+  }, []);
+
   return (
     <div className='hero'>
       <div className='hero-container'>
         <h1 className='hero-title'>
-          I'm Maqsud, a MERN stack developer based in Tashkent. Available for
-          freeance & collaborations.
+          I'm Maqsud, a MERN stack developer based in Tashkent.{' '}
+          <span className='underline'>Available</span> for freeance &
+          collaborations.
         </h1>
-        <img className='hero-img' src={heroImg} />
+        <img ref={imgRef} className='hero-img' src={heroImg} />
       </div>
     </div>
   );
