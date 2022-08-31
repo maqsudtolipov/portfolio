5a7537b04368666fdfca4fb3682a948cd8240d14
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-31 18:11:07 +0500

    Add cursor animatoin to project image

--- src/components/Portfolio/PortfolioProject.jsx
+++ src/components/Portfolio/PortfolioProject.jsx
@@ -1,10 +1,31 @@
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+
 import './PortfolioProject.scss';
 
 const PortfolioProject = (props) => {
+  // refs
+  const imgRef = useRef();
+
+  useEffect(() => {
+    const cursor = document.querySelector('.cursor');
+
+    // animate cursor
+    imgRef.current.addEventListener('mouseover', () => {
+      cursor.innerHTML = 'View project';
+      cursor.classList.add('c-project');
+
+    });
+    imgRef.current.addEventListener('mouseout', () => {
+      cursor.innerHTML = '';
+      cursor.classList.remove('c-project');
+    });
+  }, []);
+
   return (
     <div className='portfolio-project'>
       <p>{props.title}</p>
-      <img src={props.img} alt='' />
+      <img ref={imgRef} className='portfolio-project__img' src={props.img} alt='' />
     </div>
   );
 };
