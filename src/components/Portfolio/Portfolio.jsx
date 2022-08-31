3a2e02722f0c5d050344f4dab68569985f8ac132
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-31 18:18:32 +0500

    Change projects grid column to 2

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -15,6 +15,18 @@ const projects = [
     title: 'Project 3',
     img: 'https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
   },
+  {
+    title: 'Project 1',
+    img: 'https://images.unsplash.com/photo-1634118931958-f1cf1f9c6156?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+  },
+  {
+    title: 'Project 2',
+    img: 'https://images.unsplash.com/photo-1637789594401-5a0dac0d3e36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=694&q=80',
+  },
+  {
+    title: 'Project 3',
+    img: 'https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80',
+  },
 ];
 
 const Portfolio = () => {
