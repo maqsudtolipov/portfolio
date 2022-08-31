d85a136e41899e8061105dcf94057fe55d8ad1ed
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-31 17:13:02 +0500

    Fix cursor scroll bug

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -5,8 +5,8 @@ import './Cursor.scss';
 const Cursor = () => {
   const cursorRef = useRef();
   document.addEventListener('mousemove', (e) => {
-    cursorRef.current.style.top = e.pageY + 'px';
-    cursorRef.current.style.left = e.pageX + 'px';
+    cursorRef.current.style.top = e.clientY + 'px';
+    cursorRef.current.style.left = e.clientX + 'px';
   });
 
   return <div ref={cursorRef} className='cursor'></div>;
