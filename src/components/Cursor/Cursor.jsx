421e8de6e89a3ecd4c346828295d3c1694df1265
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-31 18:15:13 +0500

    Fix cursor slow speed bug

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -1,13 +1,16 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 import './Cursor.scss';
 
 const Cursor = () => {
   const cursorRef = useRef();
-  document.addEventListener('mousemove', (e) => {
-    cursorRef.current.style.top = e.clientY + 'px';
-    cursorRef.current.style.left = e.clientX + 'px';
-  });
+
+  useEffect(() => {
+    document.addEventListener('mousemove', (e) => {
+      cursorRef.current.style.top = e.clientY + 'px';
+      cursorRef.current.style.left = e.clientX + 'px';
+    });
+  }, []);
 
   return <div ref={cursorRef} className='cursor'></div>;
 };
