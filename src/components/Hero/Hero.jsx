dee8f6a417c4a83d1cded0b3cd5ea6a3c5003c8e
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-31 14:40:00 +0500

    Add text animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,7 @@ import heroImg from '../../assets/image.png';
 const Hero = () => {
   // refs
   const imgRef = useRef();
+  const titleRef = useRef();
 
   useEffect(() => {
     const tl = gsap.timeline();
@@ -14,6 +15,20 @@ const Hero = () => {
     // animate image
     tl.fromTo(
       imgRef.current,
+      {
+        opacity: 0,
+        y: '0%',
+      },
+      {
+        duration: 1,
+        opacity: 1,
+        y: '-50%',
+      }
+    );
+
+    // animate text
+    tl.fromTo(
+      titleRef.current,
       {
         opacity: 0,
         y: '10%',
@@ -29,7 +44,7 @@ const Hero = () => {
   return (
     <div className='hero'>
       <div className='hero-container'>
-        <h1 className='hero-title'>
+        <h1 ref={titleRef} className='hero-title'>
           I'm Maqsud, a MERN stack developer based in Tashkent.{' '}
           <span className='underline'>Available</span> for freeance &
           collaborations.
