d80fdc480b120e635a60cb26afb81d2ad4d7825b
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-31 16:59:11 +0500

    Create Portfolio component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.scss';
 import Hero from './components/Hero/Hero';
 import Cursor from './components/Cursor/Cursor';
+import Portfolio from './components/Portfolio/Portfolio';
 
 function App() {
   return (
     <div className='App'>
       <Hero />
+      <Portfolio />
       <Cursor />
     </div>
   );
