202cb489c831cdafed0e087978715d90682e4d69
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-31 23:46:32 +0500

    Fix scrollTrigger bug

--- src/components/UI/Heading.jsx
+++ src/components/UI/Heading.jsx
@@ -1,9 +1,43 @@
+import { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import './Heading.scss';
+gsap.registerPlugin(ScrollTrigger);
 
 const Heading = (props) => {
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    if (!titleRef.current) return;
+    console.log(titleRef.current);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: titleRef.current,
+        start: 'center bottom-=100px',
+        markers: true,
+      },
+    });
+    tl.fromTo(
+      titleRef.current,
+      {
+        y: 100,
+        duration: 1000,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+      }
+    );
+  }, []);
+
   return (
     <div className='heading'>
-      <p className='heading-title'>{props.children}</p>
+      <p ref={titleRef} className='heading-title'>
+        {props.children}
+      </p>
     </div>
   );
 };
