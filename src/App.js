e615d892567aaa10943fb551af4b9ffca76c8b9b
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-09-02 15:29:59 +0500

    Add overay animation on page load

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import gsap from 'gsap';
+
 import logo from './logo.svg';
 import './App.scss';
 import Hero from './components/Hero/Hero';
@@ -7,6 +10,27 @@ import Container from './components/UI/Container';
 import About from './components/About/About';
 
 function App() {
+  useEffect(() => {
+    gsap.to('.first', {
+      height: 0,
+    });
+
+    gsap.to('.second', {
+      height: 0,
+      delay: 0.25,
+    });
+
+    gsap.to('.third', {
+      height: 0,
+      delay: 0.5,
+    });
+
+    gsap.to('.fourth', {
+      height: 0,
+      delay: 0.75,
+    });
+  }, []);
+
   return (
     <div className='App'>
       <Hero />
@@ -17,6 +41,12 @@ function App() {
         <About />
       </Container>
       <Cursor />
+
+      {/* animation overlay */}
+      <div className='overlay first'></div>
+      <div className='overlay second'></div>
+      <div className='overlay third'></div>
+      <div className='overlay fourth'></div>
     </div>
   );
 }
