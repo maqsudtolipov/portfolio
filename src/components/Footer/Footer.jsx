3bf08a2ef730a9d6bbd22e8bc4f540d8bc56c09f
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-09-03 12:58:04 +0500

    Add copyright text to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,20 @@
+import { IoLogoGithub, IoLogoTwitter, IoLogoDiscord } from 'react-icons/io5';
 import logo from '../../assets/logo.svg';
-import './Footer.scss'
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>
+    <div className='footer'>
       <img className='footer-logo' src={logo} alt='' />
+      <p className='footer-text'>
+        Copyright &copy; {new Date().getFullYear()} by Maqsud. All rights
+        reserved.
+      </p>
+      <div className='footer-icons'>
+        <IoLogoGithub className='footer-icon' />
+        <IoLogoTwitter className='footer-icon' />
+        <IoLogoDiscord className='footer-icon' />
+      </div>
     </div>
   );
 };
