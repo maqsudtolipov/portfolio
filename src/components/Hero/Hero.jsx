44be30262ef1fda03a6cc017691c457af0ca5d55
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-09-07 22:03:06 +0500

    Add hero text animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,6 +8,7 @@ const Hero = () => {
   // refs
   const imgRef = useRef();
   const titleRef = useRef();
+  const backRef = useRef();
 
   useEffect(() => {
     const tl = gsap.timeline({
@@ -45,6 +46,26 @@ const Hero = () => {
       '<50%'
     );
 
+    const backTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: backRef.current,
+        toggleActions: 'play none none none',
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: true,
+
+      },
+    });
+
+    backTl.fromTo(
+        backRef.current,
+        {
+          left: '100%'
+        }, {
+          left: '-20%',
+        }
+    )
+
     // cursor animate
     titleRef.current.addEventListener('mouseover', () =>
       cursor.classList.add('c-title')
@@ -58,10 +79,13 @@ const Hero = () => {
     <div className='hero'>
       <div className='hero-container'>
         <h1 ref={titleRef} className='hero-title'>
-          FULL STACK DEV spends his days building tools that help make the Internet a better place for everyone 🌍
+          <span className='hero-title-highlight'>FULL STACK DEV</span>&nbsp;
+          spends his days building tools that help make the Internet a better place for everyone
         </h1>
         <img ref={imgRef} className='hero-img' src={heroImg} alt='' />
       </div>
+
+      <p ref={backRef} className='back-text'>FULL STACK FULL STACK FULL STACK FULL STACK FULL STACK</p>
     </div>
   );
 };
