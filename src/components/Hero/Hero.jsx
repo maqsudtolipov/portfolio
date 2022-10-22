eb898aa2db43c9ac8f01842a1fc5d4112c3c9782
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-10-22 09:39:39 +0500

    Optimize website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,8 @@
-import { useRef, useEffect } from 'react';
-import { gsap } from 'gsap';
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
-import './Hero.scss';
-import heroImg from '../../assets/image.png';
+import "./Hero.scss";
+import heroImg from "../../assets/hero.webp";
 
 const Hero = () => {
   // refs
@@ -14,19 +14,19 @@ const Hero = () => {
     const tl = gsap.timeline({
       delay: 1.5,
     });
-    const cursor = document.querySelector('.cursor');
+    const cursor = document.querySelector(".cursor");
 
     // animate image
     tl.fromTo(
       imgRef.current,
       {
         opacity: 0,
-        y: '0%',
+        y: "0%",
       },
       {
         duration: 1,
         opacity: 1,
-        y: '-50%',
+        y: "-50%",
       }
     );
 
@@ -34,24 +34,24 @@ const Hero = () => {
     tl.fromTo(
       titleRef.current,
       {
-        y: '50%',
+        y: "50%",
         skewY: 10,
         opacity: 0,
       },
       {
-        y: '0%',
+        y: "0%",
         skewY: 0,
         opacity: 1,
       },
-      '<50%'
+      "<50%"
     );
 
     const backTl = gsap.timeline({
       scrollTrigger: {
         trigger: backRef.current,
-        toggleActions: 'play none none none',
-        start: 'top bottom',
-        end: 'bottom top',
+        toggleActions: "play none none none",
+        start: "top bottom",
+        end: "bottom top",
         scrub: true,
       },
     });
@@ -59,34 +59,34 @@ const Hero = () => {
     backTl.fromTo(
       backRef.current,
       {
-        left: '100%',
+        left: "100%",
       },
       {
-        left: '-20%',
+        left: "-20%",
       }
     );
 
     // cursor animate
-    titleRef.current.addEventListener('mouseover', () =>
-      cursor.classList.add('c-title')
+    titleRef.current.addEventListener("mouseover", () =>
+      cursor.classList.add("c-title")
     );
-    titleRef.current.addEventListener('mouseout', () =>
-      cursor.classList.remove('c-title')
+    titleRef.current.addEventListener("mouseout", () =>
+      cursor.classList.remove("c-title")
     );
   }, []);
 
   return (
-    <div className='hero'>
-      <div className='hero-container'>
-        <h1 ref={titleRef} className='hero-title'>
-          <span className='hero-title-highlight'>FULL STACK DEV</span>&nbsp;
+    <div className="hero">
+      <div className="hero-container">
+        <h1 ref={titleRef} className="hero-title">
+          <span className="hero-title-highlight">FULL STACK DEV</span>&nbsp;
           spends his days building tools that help make the Internet a better
           place for everyone
         </h1>
-        <img ref={imgRef} className='hero-img' src={heroImg} alt='' />
+        <img ref={imgRef} className="hero-img" src={heroImg} alt="" />
       </div>
 
-      <p ref={backRef} className='back-text'>
+      <p ref={backRef} className="back-text">
         FULL STACK FULL STACK FULL STACK FULL STACK FULL STACK
       </p>
     </div>
