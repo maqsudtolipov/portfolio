52eac5629ef7dc7a649c91271a7401ceae9ada22
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-10-03 23:40:27 +0500

    ☘️ Edit spotify page

--- src/Pages/Spotify.jsx
+++ src/Pages/Spotify.jsx
@@ -1,48 +1,90 @@
 import "./Pages.scss";
-import {IoGlobe, IoLogoGithub} from "react-icons/io5";
-import {Link} from "react-router-dom";
+import { IoGlobe, IoLogoGithub } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
-import img1 from './../assets/spotify/spotify-1.png'
+import img1 from "./../assets/spotify/spotify-1.png";
+import img2 from "./../assets/spotify/spotify-2.png";
+import img3 from "./../assets/spotify/spotify-3.png";
 
 const Spotify = () => {
   return (
-    <div className='page'>
-        <div className="page__glow"></div>
-        <div className="page__title">
-
+    <div className="page">
+      <div className="page__glow"></div>
+      <div className="page__title">
         <h1 className="pg-heading-primary">Spotify Clone</h1>
-            <div className="page__title-icons">
-                <Link to='https://github.com/maqsudtolipov/spotify-clone'>
-                    <IoLogoGithub />
-                </Link>
-                <Link to='https://maqsud-spotify.vercel.app/signup'>
-                    <IoGlobe />
-                </Link>
-            </div>
+        <div className="page__title-icons">
+          <Link to="https://github.com/maqsudtolipov/spotify-clone">
+            <IoLogoGithub />
+          </Link>
+          <Link to="https://maqsud-spotify.vercel.app/signup">
+            <IoGlobe />
+          </Link>
         </div>
-        <img src={img1} />
-        <ul className="page__tags">
-            <li className="page__tag" style={{color: '#00ed64'}}>MongoDB</li>
-            <li className="page__tag" style={{color: '#a2a2a2'}}>Express</li>
-            <li className="page__tag" style={{color: '#61DBFB'}}>React</li>
-            <li className="page__tag" style={{color: '#66cc33'}}>NodeJs</li>
-            <li className="page__tag" style={{color: '#CC6699'}}>SCSS</li>
-            <li className="page__tag" style={{color: '#764abc'}}>Redux</li>
-            <li className="page__tag" style={{color: '#f44250'}}>React Router</li>
-            <li className="page__tag" style={{color: '#B7B7FF'}}>ESLint</li>
-        </ul>
+      </div>
+      <img src={img1} />
+      <ul className="page__tags">
+        <li className="page__tag" style={{ color: "#00ed64" }}>
+          MongoDB
+        </li>
+        <li className="page__tag" style={{ color: "#a2a2a2" }}>
+          Express
+        </li>
+        <li className="page__tag" style={{ color: "#61DBFB" }}>
+          React
+        </li>
+        <li className="page__tag" style={{ color: "#66cc33" }}>
+          NodeJs
+        </li>
+        <li className="page__tag" style={{ color: "#CC6699" }}>
+          SCSS
+        </li>
+        <li className="page__tag" style={{ color: "#764abc" }}>
+          Redux
+        </li>
+        <li className="page__tag" style={{ color: "#f44250" }}>
+          React Router
+        </li>
+        <li className="page__tag" style={{ color: "#B7B7FF" }}>
+          ESLint
+        </li>
+      </ul>
 
-        <h2 className="pg-heading-secondary">Project Features</h2>
-        <p className="pg-paragraph">Here're some of the project's best features:</p>
-        <ul className='page__list'>
-            <li>User authentication and authorization</li>
-            <li>Search functionality to find songs playlists and artists</li>
-            <li>Create, edit and delete playlists</li>
-            <li>Play songs and control playback (play pause skip etc.)</li>
-            <li>Admin page for managing songs and see statistics</li>
-            <li>Listen to songs in your queue</li>
-            <li>Control player with MacBook Pro touch bar</li>
-        </ul>
+      <h2 className="pg-heading-secondary">Project Features</h2>
+      <p className="pg-paragraph">
+        Here're some of the project's best features:
+      </p>
+      <ul className="page__list">
+        <li>User authentication and authorization</li>
+        <li>Search functionality to find songs playlists and artists</li>
+        <li>Create, edit and delete playlists</li>
+        <li>Play songs and control playback (play pause skip etc.)</li>
+        <li>Admin page for managing songs and see statistics</li>
+        <li>Listen to songs in your queue</li>
+        <li>Control player with MacBook Pro touch bar</li>
+      </ul>
+
+      <h2 className="pg-heading-secondary">Screenshots</h2>
+      <div className="pg-gallery-2">
+        <img className="pg-img" src={img2} />
+        <img className="pg-img" src={img3} />
+      </div>
+
+      <h2 className="pg-heading-secondary">Experience</h2>
+      <p className="pg-paragraph">
+        I recently completed my first full-stack project, which I started after
+        finishing the NodeJS course by Jonas Schmedtmann. It took me four months
+        to complete, with breaks for studying during the summer. Throughout this
+        project, I gained a wealth of knowledge and had an amazing experience.
+        Managing the different aspects of the app, such as the Database, API,
+        Frontend, and Media storage, felt like working on a real-world project,
+        even though I don't have much work experience. While there were
+        challenges, I encountered and fixed many bugs, some of which took hours
+        or even days to resolve. However, I'm now equipped with the skills to
+        fix similar bugs in just five minutes next time. As part of this
+        project, I also created my own CSS style guide, which I plan to share in
+        the future. Overall, this project was a valuable learning experience for
+        me.
+      </p>
       {/*<h1 className="pg-heading-primary">Heading 1</h1>*/}
       {/*<h2 className="pg-heading-secondary">Heading 2</h2>*/}
       {/*<h3 className="pg-heading-tertiary">Heading 3</h3>*/}
