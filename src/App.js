3c078a969cd9b488ee577589a0feb370cb06dc8c
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2023-12-25 23:02:19 +0500

    😢 Shut down portfolio

--- src/App.js
+++ src/App.js
@@ -14,156 +14,157 @@ import Nav from "./components/Nav/Nav";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Spotify from "./Pages/Spotify";
 import Instagram from "./Pages/Instagram";
+import Soon from "./components/Soon";
 
 function App() {
-  useEffect(() => {
-    const tl = gsap.timeline({});
-    const buttons = document.querySelectorAll("button");
-    const inputs = document.querySelectorAll("input");
-    const textareas = document.querySelectorAll("textarea");
-    const cursor = document.querySelector(".cursor");
-    const footerLinks = document.querySelectorAll(".footer-icon");
-
-    tl.to(".first", {
-      width: 0,
-      delay: 0.5,
-    });
-
-    tl.to(
-      ".second",
-      {
-        width: 0,
-      },
-      "<0%",
-    );
-
-    tl.to(
-      ".third",
-      {
-        width: 0,
-      },
-      "<0%",
-    );
-
-    tl.to(
-      ".fourth",
-      {
-        width: 0,
-      },
-      "<0%",
-    );
-
-    tl.to(".overlay", {
-      border: "0px solid black",
-    });
-
-    console.log(buttons);
-
-    buttons.forEach((button) => {
-      button.addEventListener("mouseover", () =>
-        cursor.classList.add("c-none"),
-      );
-      button.addEventListener("mouseout", () =>
-        cursor.classList.remove("c-none"),
-      );
-    });
-
-    inputs.forEach((input) => {
-      input.addEventListener("mouseover", () => cursor.classList.add("c-none"));
-      input.addEventListener("mouseout", () =>
-        cursor.classList.remove("c-none"),
-      );
-    });
-
-    textareas.forEach((textarea) => {
-      textarea.addEventListener("mouseover", () =>
-        cursor.classList.add("c-none"),
-      );
-      textarea.addEventListener("mouseout", () =>
-        cursor.classList.remove("c-none"),
-      );
-    });
-
-    footerLinks.forEach((link) => {
-      link.addEventListener("mouseover", () => cursor.classList.add("c-none"));
-      link.addEventListener("mouseout", () =>
-        cursor.classList.remove("c-none"),
-      );
-    });
-  }, []);
+  // useEffect(() => {
+  //   const tl = gsap.timeline({});
+  //   const buttons = document.querySelectorAll("button");
+  //   const inputs = document.querySelectorAll("input");
+  //   const textareas = document.querySelectorAll("textarea");
+  //   const cursor = document.querySelector(".cursor");
+  //   const footerLinks = document.querySelectorAll(".footer-icon");
+  //
+  //   tl.to(".first", {
+  //     width: 0,
+  //     delay: 0.5,
+  //   });
+  //
+  //   tl.to(
+  //     ".second",
+  //     {
+  //       width: 0,
+  //     },
+  //     "<0%",
+  //   );
+  //
+  //   tl.to(
+  //     ".third",
+  //     {
+  //       width: 0,
+  //     },
+  //     "<0%",
+  //   );
+  //
+  //   tl.to(
+  //     ".fourth",
+  //     {
+  //       width: 0,
+  //     },
+  //     "<0%",
+  //   );
+  //
+  //   tl.to(".overlay", {
+  //     border: "0px solid black",
+  //   });
+  //
+  //   console.log(buttons);
+  //
+  //   buttons.forEach((button) => {
+  //     button.addEventListener("mouseover", () =>
+  //       cursor.classList.add("c-none"),
+  //     );
+  //     button.addEventListener("mouseout", () =>
+  //       cursor.classList.remove("c-none"),
+  //     );
+  //   });
+  //
+  //   inputs.forEach((input) => {
+  //     input.addEventListener("mouseover", () => cursor.classList.add("c-none"));
+  //     input.addEventListener("mouseout", () =>
+  //       cursor.classList.remove("c-none"),
+  //     );
+  //   });
+  //
+  //   textareas.forEach((textarea) => {
+  //     textarea.addEventListener("mouseover", () =>
+  //       cursor.classList.add("c-none"),
+  //     );
+  //     textarea.addEventListener("mouseout", () =>
+  //       cursor.classList.remove("c-none"),
+  //     );
+  //   });
+  //
+  //   footerLinks.forEach((link) => {
+  //     link.addEventListener("mouseover", () => cursor.classList.add("c-none"));
+  //     link.addEventListener("mouseout", () =>
+  //       cursor.classList.remove("c-none"),
+  //     );
+  //   });
+  // }, []);
 
   return (
     <div className="App">
       <BrowserRouter>
-        {/* Instagram */}
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Nav />
-                <Hero />
-                <Container>
-                  <Portfolio />
-                </Container>
-                <Container>
-                  <About />
-                </Container>
-                <Container>
-                  <Contact />
-                </Container>
-                <Footer />
-                <Cursor />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/aa"
-            element={
-              <>
-                <Nav />
-                <Hero />
-                <Container>
-                  <Spotify />
-                </Container>
-                <Footer />
-                <Cursor />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/projects/spotify"
-            element={
-              <>
-                <Nav />
-                <Container>
-                  <Spotify />
-                </Container>
-                <Footer />
-                <Cursor />
-              </>
-            }
-          ></Route>
-          <Route
-            path="/projects/instagram"
-            element={
-              <>
-                <Nav />
-                <Container>
-                  <Instagram />
-                </Container>
-                <Footer />
-                <Cursor />
-              </>
-            }
-          ></Route>
+          {/*<Route*/}
+          {/*  path="/"*/}
+          {/*  element={*/}
+          {/*    <>*/}
+          {/*      <Nav />*/}
+          {/*      <Hero />*/}
+          {/*      <Container>*/}
+          {/*        <Portfolio />*/}
+          {/*      </Container>*/}
+          {/*      <Container>*/}
+          {/*        <About />*/}
+          {/*      </Container>*/}
+          {/*      <Container>*/}
+          {/*        <Contact />*/}
+          {/*      </Container>*/}
+          {/*      <Footer />*/}
+          {/*      <Cursor />*/}
+          {/*    </>*/}
+          {/*  }*/}
+          {/*></Route>*/}
+          {/*<Route*/}
+          {/*  path="/aa"*/}
+          {/*  element={*/}
+          {/*    <>*/}
+          {/*      <Nav />*/}
+          {/*      <Hero />*/}
+          {/*      <Container>*/}
+          {/*        <Spotify />*/}
+          {/*      </Container>*/}
+          {/*      <Footer />*/}
+          {/*      <Cursor />*/}
+          {/*    </>*/}
+          {/*  }*/}
+          {/*></Route>*/}
+          {/*<Route*/}
+          {/*  path="/projects/spotify"*/}
+          {/*  element={*/}
+          {/*    <>*/}
+          {/*      <Nav />*/}
+          {/*      <Container>*/}
+          {/*        <Spotify />*/}
+          {/*      </Container>*/}
+          {/*      <Footer />*/}
+          {/*      <Cursor />*/}
+          {/*    </>*/}
+          {/*  }*/}
+          {/*></Route>*/}
+          {/*<Route*/}
+          {/*  path="/projects/instagram"*/}
+          {/*  element={*/}
+          {/*    <>*/}
+          {/*      <Nav />*/}
+          {/*      <Container>*/}
+          {/*        <Instagram />*/}
+          {/*      </Container>*/}
+          {/*      <Footer />*/}
+          {/*      <Cursor />*/}
+          {/*    </>*/}
+          {/*  }*/}
+          {/*></Route>*/}
+          <Route path="*" element={<Soon />} />
         </Routes>
 
         {/* animation overlay */}
-        <div className="overlay first"></div>
-        <div className="overlay second"></div>
-        <div className="overlay third"></div>
-        <div className="overlay fourth"></div>
+        {/*<div className="overlay first"></div>*/}
+        {/*<div className="overlay second"></div>*/}
+        {/*<div className="overlay third"></div>*/}
+        {/*<div className="overlay fourth"></div>*/}
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Soon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Soon = () => {
+  return (
+    <div className="page">
+      <h1>Soon I will finish this website</h1>
+      <p>
+        Until then contact me{" "}
+        <a href="mailto:maqsudtolipov9@gmail.com">maqsudtolipov9@gmail.com</a>
+      </p>
+    </div>
+  );
+};
+
+export default Soon;
